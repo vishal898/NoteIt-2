3aeae10de0ee12043807d0ba7e5e741bbd711e36
sweet1125 <sweetygupta4605@gmail.com>
2021-10-04 20:02:55 +0530

    auth-config file created

---
+++ backend/config/auth-config.js
@@ -0,0 +1,52 @@
+const User = require("../models/user");
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+module.exports = (passport) => {
+	passport.serializeUser(function (user, done) {
+		done(null, user);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		// User.findById(id, function (err, user) {
+		done(null, id);
+		// });
+	});
+
+	passport.use(
+		new GoogleStrategy(
+			{
+				clientID:
+					"454012695094-ee81ef88ch3q4ij4r6el51fvn34hvie3.apps.googleusercontent.com",
+				clientSecret: "casob4pGsPYiAsA_CVuhe-HJ",
+				callbackURL: "http://localhost:5000/google/callback",
+			},
+			function (accessToken, refreshToken, profile, cb) {
+				//
+
+				User.findOne({ googleID: profile.id }, async (err, user) => {
+					if (err) throw err;
+
+					//
+					if (user) {
+						//
+						cb(null, user);
+					} else {
+						const newUser = new User({
+							// googleID: profile.id,
+							email: profile.emails[0].value,
+							username: profile.displayName,
+							// phoneNo: null,
+							// username: null,
+						});
+						await newUser.save();
+						//
+						cb(null, newUser);
+					}
+				});
+			}
+		)
+	);
+};
+
+dotenv.config({ path: './config.env' });
+const CLIENTID = process.env.CLIENTID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
