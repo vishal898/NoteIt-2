57668916b1275c4d9f350cd8b4268d92727a782a
vishal898 <vishal.borse898@gmail.com>
2021-10-12 13:36:57 +0530

    updated

--- frontend/src/pages/User/user.js
+++ frontend/src/pages/User/user.js
@@ -1,16 +1,34 @@
 import Navbar from '../../Components/Navbar/Navbar';
+import Menu from './menuApi';
+import React from 'react';
+import './user.css';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 const User = () => {
+
+  const [menuData, setMenuData] = React.useState(Menu);
+  const largerThanSixty = Menu.filter( number => {
+    return number.googleID == "vishalg"
+  })
+
   return (
     <>
     <Navbar/>
-   
-    <div className="container">
-    
-      <h1 className="text-center" style={{paddingTop: "30%"}}>
-        user
-      </h1>
+
+
+      <div  >
+        <div className="card">
       
-    </div>
+          <h1 className="username">{ largerThanSixty.map(number => <li>{number.username}</li>) }</h1>
+          <p className="title">{largerThanSixty.map(number => <li>{number.email}</li>) }</p>
+          <h2 className="yourtag">Note Count</h2>
+          <button className="buttonhome">{ largerThanSixty.map(number => <li>{number.notes.length}</li>) }</button>
+          <h2 className="yourtag">Your Tags</h2>
+          <p className="tags">{ largerThanSixty.map(number => <li>{number.tags.toString()}</li>) }</p>
+
+          <p><button>Logout</button></p>
+        </div>
+       
+      </div>
     </>
   )
 }
