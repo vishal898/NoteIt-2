efa5896d604cf17b5546c73f9a6c44fefbed54c5
vishal898 <vishal.borse898@gmail.com>
2021-11-04 14:20:56 +0530

    merge

--- backend/models/note.js
+++ backend/models/note.js
@@ -22,11 +22,7 @@ const noteSchema = new Schema({
 		type: Date,
 		required: true,
 	},
-<<<<<<< HEAD
-    tags: [String],
-=======
     tags:[String],
->>>>>>> 3a6d5a4b22232e1f9e86657cbca0a9320757bf82
     visitCnt:{
         type:Number,
     },
